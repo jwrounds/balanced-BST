86384aad4b5e6d2488dd843c180cd58dee77b327
Jason Rounds <jwrounds@usfca.edu>
2020-11-20 13:18:24 -0600

    Implemented Node class, and structure of Tree class with build_tree method, plus visualization method

---
+++ bst.rb
@@ -0,0 +1,79 @@
+class Node
+  attr_accessor :data, :left, :right
+  def initialize data
+    @data = data
+    @left = nil
+    @right = nil
+  end
+end
+
+class Tree
+  def initialize array
+    @root = self.build_tree(array)
+  end
+
+  def build_tree array, start = 0, finish = array.length - 1
+    return nil if start > finish
+    
+    mid = (start + finish) / 2
+    root = Node.new(array[mid])
+
+    root.left = self.build_tree(array, start, mid - 1)
+    root.right = self.build_tree(array, mid + 1, finish)
+
+    return root
+  end
+
+  def insert value
+
+  end
+
+  def delete value
+
+  end
+
+  def find value
+    
+  end
+
+  def level_order
+
+  end
+
+  def preorder
+
+  end
+
+  def inorder
+
+  end
+
+  def postorder
+
+  end
+
+  def height node
+
+  end
+
+  def depth node
+
+  end
+
+  def balanced? tree
+
+  end
+
+  def balance tree
+
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+end
+
+tree = Tree.new([1, 2, 3, 4, 5, 6, 7])
+tree.pretty_print
