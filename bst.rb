b9c5ace032291bfa371fee2bef153456afc21d68
Jason Rounds <jwrounds@usfca.edu>
2020-11-24 16:18:53 -0600

    Refactored delete method to account for branch or leaf nodes of successor when removing target node

--- bst.rb
+++ bst.rb
@@ -41,8 +41,58 @@ class Tree
     return node
   end
 
-  def delete value
+  def find_successor sub_root
+    successor = sub_root
 
+    while successor.left != nil
+      successor_branch = successor
+      successor = successor.left
+    end
+
+    if successor_branch
+      # connect subroot node to children of successor node if nay exist
+      successor_branch.left = successor.right
+      successor_branch.left.left = successor.left if successor.left
+    end
+    
+    return successor
+  end
+
+  def delete value, node = self.root
+    return nil if node == nil
+    if node.data == value
+    # node has two children
+      if node.left && node.right
+      # find inorder successor
+      sub_root_right = node.right
+      sub_root_left = node.left
+
+      successor = self.find_successor(sub_root_right)
+
+      puts "sub-root-right: #{sub_root_right.data} sub-root-left: #{sub_root_left.data} successor: #{successor.data} node: #{node.data}"
+
+      # reassign successor node connections to left and right subtrees
+      successor.left = sub_root_left unless successor == sub_root_left
+      successor.right = sub_root_right unless successor == sub_root_right
+      # reassign Tree instance's root variable to sucessor if it replaced the root node
+      self.root = successor if node == self.root
+
+      return successor
+    # node has one child
+      elsif node.left
+        return node.left
+      elsif node.right
+        return node.right
+      else
+    # node is leaf
+        return nil
+      end
+    end
+
+    node.left = self.delete(value, node.left)
+    node.right = self.delete(value, node.right)
+
+    return node
   end
 
   def find value
@@ -88,7 +138,11 @@ class Tree
   end
 end
 
-tree = Tree.new([1, 2, 3, 4, 5, 6, 7])
-tree.insert 8
+tree = Tree.new([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 20, 21, 23, 26])
+tree.pretty_print
+
+tree.delete 2
+tree.delete 6
+tree.delete 14
 
 tree.pretty_print
